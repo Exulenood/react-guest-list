826f06b7bb0ac477dc6ccad1e5154f604925a625
Exulenood <119940832+Exulenood@users.noreply.github.com>
2023-01-26 14:47:42 +0100

    initialize Project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const guest = [
+  {
+    firstName: 'Pepel',
+    secondName: 'Bair',
+    attending: false,
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <label>
+        First Name:
+        <input type="text" value />
+      </label>
+    </div>
+  );
+}
